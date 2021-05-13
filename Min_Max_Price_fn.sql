2da822e7f4b09d2af68af1e0bfe6c3245463809b
Jenith Devabakthini <jenith.d@gmail.com>
2021-05-13 07:18:43 -0700

    changed max min price between dates

--- Min_Max_Price_fn.sql
+++ Min_Max_Price_fn.sql
@@ -2,7 +2,7 @@
 --  DDL for Function MIN_MAX_PRICE_FN
 --------------------------------------------------------
 
-  CREATE OR REPLACE FUNCTION "NASDAQ"."MIN_MAX_PRICE_FN" (in_symbol in varchar2,Minmax in varchar2,years  IN number)
+ create or replace function Min_Max_Price_fn(in_symbol in varchar2,Minmax in varchar2,years  IN number)
 RETURN maxmin_fn_type
 is
 v_number number;
@@ -13,21 +13,21 @@ rw_cnt number;
 begin
 if (Minmax='MIN') then
 if (years=1) then
-select currentprice,min(tradedate) into v_currentprice,v_date from nasdaq_avg where (symbol,currentprice) in (select symbol,min(currentprice) from nasdaq_avg where symbol=in_symbol AND TRADEDATE>(SELECT MAX(TRADEDATE)-365  FROM NASDAQ_AVG )  GROUP BY SYMBOL) group by currentprice;
+select currentprice,min(tradedate) into v_currentprice,v_date from nasdaq_avg where (symbol,currentprice) in (select symbol,min(currentprice) from nasdaq_avg where symbol=in_symbol AND TRADEDATE between (SELECT MAX(TRADEDATE)-365  FROM NASDAQ_AVG ) and (SELECT MAX(TRADEDATE)  FROM NASDAQ_AVG )  GROUP BY SYMBOL) group by currentprice;
 elsif (years=2) then
-select currentprice,min(tradedate) into v_currentprice,v_date from nasdaq_avg where (symbol,currentprice) in (select symbol,min(currentprice) from nasdaq_avg where symbol=in_symbol AND TRADEDATE>(SELECT MAX(TRADEDATE)-730  FROM NASDAQ_AVG )  GROUP BY SYMBOL) group by currentprice;
+select currentprice,min(tradedate) into v_currentprice,v_date from nasdaq_avg where (symbol,currentprice) in (select symbol,min(currentprice) from nasdaq_avg where symbol=in_symbol AND TRADEDATE between (SELECT MAX(TRADEDATE)-730  FROM NASDAQ_AVG ) and (SELECT MAX(TRADEDATE)-365  FROM NASDAQ_AVG ) GROUP BY SYMBOL) group by currentprice;
 elsif (years=3) then
-select currentprice,min(tradedate) into v_currentprice,v_date from nasdaq_avg where (symbol,currentprice) in (select symbol,min(currentprice) from nasdaq_avg where symbol=in_symbol AND TRADEDATE>(SELECT MAX(TRADEDATE)-1095  FROM NASDAQ_AVG )  GROUP BY SYMBOL) group by currentprice;
+select currentprice,min(tradedate) into v_currentprice,v_date from nasdaq_avg where (symbol,currentprice) in (select symbol,min(currentprice) from nasdaq_avg where symbol=in_symbol AND TRADEDATE between (SELECT MAX(TRADEDATE)-1195  FROM NASDAQ_AVG ) and (SELECT MAX(TRADEDATE)-730  FROM NASDAQ_AVG ) GROUP BY SYMBOL) group by currentprice;
 else
 null;
 end if;
 elsif (Minmax='MAX') then
 if (years=1) then
-select currentprice,max(tradedate) into v_currentprice,v_date from nasdaq_avg where (symbol,currentprice) in (select symbol,max(currentprice) from nasdaq_avg where symbol=in_symbol AND TRADEDATE>(SELECT MAX(TRADEDATE)-365  FROM NASDAQ_AVG )  GROUP BY SYMBOL) group by currentprice;
+select currentprice,max(tradedate) into v_currentprice,v_date from nasdaq_avg where (symbol,currentprice) in (select symbol,max(currentprice) from nasdaq_avg where symbol=in_symbol AND TRADEDATE between (SELECT MAX(TRADEDATE)-365  FROM NASDAQ_AVG ) and (SELECT MAX(TRADEDATE)  FROM NASDAQ_AVG ) GROUP BY SYMBOL) group by currentprice;
 elsif (years=2) then
-select currentprice,max(tradedate) into v_currentprice,v_date from nasdaq_avg where (symbol,currentprice) in (select symbol,max(currentprice) from nasdaq_avg where symbol=in_symbol AND TRADEDATE>(SELECT MAX(TRADEDATE)-730  FROM NASDAQ_AVG )  GROUP BY SYMBOL) group by currentprice;
+select currentprice,max(tradedate) into v_currentprice,v_date from nasdaq_avg where (symbol,currentprice) in (select symbol,max(currentprice) from nasdaq_avg where symbol=in_symbol AND TRADEDATE between (SELECT MAX(TRADEDATE)-730  FROM NASDAQ_AVG ) and (SELECT MAX(TRADEDATE)-365  FROM NASDAQ_AVG ) GROUP BY SYMBOL) group by currentprice;
 elsif (years=3) then
-select currentprice,max(tradedate) into v_currentprice,v_date from nasdaq_avg where (symbol,currentprice) in (select symbol,max(currentprice) from nasdaq_avg where symbol=in_symbol AND TRADEDATE>(SELECT MAX(TRADEDATE)-1095  FROM NASDAQ_AVG )  GROUP BY SYMBOL) group by currentprice;
+select currentprice,max(tradedate) into v_currentprice,v_date from nasdaq_avg where (symbol,currentprice) in (select symbol,max(currentprice) from nasdaq_avg where symbol=in_symbol AND TRADEDATE between (SELECT MAX(TRADEDATE)-1195  FROM NASDAQ_AVG ) and (SELECT MAX(TRADEDATE)-730  FROM NASDAQ_AVG ) GROUP BY SYMBOL) group by currentprice;
 else
 null;
 end if;
@@ -36,5 +36,3 @@ null;
 end if;
 return maxmin_fn_type(v_currentprice,v_date);
 end;
-
-/
